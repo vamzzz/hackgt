93d5078a7fa7428bf41c92548dcdc99457058cb8
coreycaskey <ccaskey6@gatech.edu>
2017-10-14 13:42:40 -0400

    Can Add Recipes And See Shopping List

--- WEB_ONLY/script.js
+++ WEB_ONLY/script.js
@@ -1,5 +1,9 @@
+var shoppingList = [] // users current shopping list of ingredients
+var currentRecipes = [] // dictionary holding list of recipes currently displayed on screen
+
 function searchFood(foodText, foodSearches) {
-	var result = []
+	currentRecipes = [] // clear past recipes held on new search
+	// var result = []
 	$.getJSON("https://api.edamam.com/search?q=" + foodText, function(data, status) {
 		var div = document.getElementById('foodResults');
 		while (div.firstChild) { div.removeChild(div.firstChild); }
@@ -8,13 +12,18 @@ function searchFood(foodText, foodSearches) {
 		for (var index = 0; index < foodSearches; index++) {
 			var resultRecipe = { ingredients: [] }
 			var recipe = recipes[index]['recipe'];
-
 			var recipeDiv = document.createElement('div');
 
 			var a = document.createElement('a');
 			var img = document.createElement('img');
 			var h3 = document.createElement('h3');
 			var ul = document.createElement('ul');
+			var button = document.createElement('button');
+			button.id = index;
+			button.addEventListener('click', function() {
+				addRecipe(this.id);
+			}, false);
+			var buttonContent = document.createTextNode("Add Recipe");
 
 			recipe['ingredientLines'].forEach(function(ingredient) {
 				var li = document.createElement('li');
@@ -30,9 +39,11 @@ function searchFood(foodText, foodSearches) {
 
 			a.appendChild(img);
 			h3.appendChild(h3Text);
+			button.appendChild(buttonContent);
 			recipeDiv.appendChild(a);
 			recipeDiv.appendChild(h3);
 			recipeDiv.appendChild(ul);
+			recipeDiv.appendChild(button);
 			div.appendChild(recipeDiv);
 
 			var firstRecipeIngredients = recipe['ingredients'];
@@ -40,10 +51,13 @@ function searchFood(foodText, foodSearches) {
 				resultRecipe.ingredients.push({quantity: i['quantity'],
 										measure: i['measure'] == '<unit>' ? "" : i['measure'],
 										food: i['food']});})
-			result.push(resultRecipe);}
-		}
-	);
-	return result;
+			// result.push(resultRecipe);}
+			currentRecipes.push(resultRecipe);
+		} // end for loop
+	});
+
+	// return result;
+	return currentRecipes; // currentRecipes now holds each recipe values
 }
 
 function getFood() {
@@ -52,3 +66,57 @@ function getFood() {
 	if (!foodText || foodSearches < 1) { alert("Please type in a valid food search or search number"); }
 	else { return searchFood(foodText, foodSearches); }
 }
+
+function addRecipe(buttonIdString) {
+	var intVal = parseInt(buttonIdString);
+	// console.log(intVal);
+	shoppingList.push(currentRecipes[intVal]);
+	setLocalStorage();
+}
+
+function getShoppingList() {
+	shoppingList = getLocalStorage();
+	var div = document.getElementById("shoppingList");
+	var ul = document.createElement("ul");
+	// console.log(shoppingList.length);
+	for (var i = 0; i < shoppingList.length; i++) {
+		for (var j = 0; j < shoppingList[i]["ingredients"].length; j++) {
+			var ingredientItem = shoppingList[i]["ingredients"][j];
+			var li = document.createElement("li");
+			var liText = document.createTextNode("(" + ingredientItem["quantity"] + " " + ingredientItem["measure"]+ ") " + ingredientItem["food"])
+			li.appendChild(liText);
+			ul.appendChild(li);
+		}
+
+	}
+	div.appendChild(ul);
+}
+
+function setLocalStorage() {
+	localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+}
+
+function getLocalStorage() {
+		var ret = JSON.parse(localStorage.getItem("shoppingList"));
+		console.log(ret);
+    return ret;
+}
+
+// function checkJSON(url) {
+// 	var AJAX_req = new XMLHttpRequest();
+//   AJAX_req.open( "GET", url, true );
+//   AJAX_req.setRequestHeader("Content-type", "application/json");
+//
+//   AJAX_req.onreadystatechange = function()
+//   {
+//       if( AJAX_req.readyState == 4 && AJAX_req.status == 200 )
+//       {
+//           var response = JSON.parse( AJAX_req.responseText );
+//           // document.write( response.name );
+// 					console.log(response.snapshot);
+//       }
+//   }
+//   AJAX_req.send();
+// }
+//
+// checkJSON( 'http://localhost:8080/WEB_ONLY/fake_data.json' );
